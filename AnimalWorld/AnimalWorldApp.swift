2991b1b8c1767300999093d33c6ee690b73d49c7
Mato <Kittawat@icloud.com>
2022-11-12 23:30:20 +0800

    Initial Commit

---
+++ AnimalWorld/AnimalWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalWorldApp.swift
+//  AnimalWorld
+//
+//  Created by kittawat phuangsombat on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct AnimalWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalWorld/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimalWorld
+//
+//  Created by kittawat phuangsombat on 2022/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
