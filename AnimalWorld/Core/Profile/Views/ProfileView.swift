8355dbbe38638126917a99bdd266d7e62c473117
Mato <Kittawat@icloud.com>
2022-11-21 13:23:02 +0800

    adjust profie to the top

--- AnimalWorld/Core/Profile/Views/ProfileView.swift
+++ AnimalWorld/Core/Profile/Views/ProfileView.swift
@@ -9,42 +9,40 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        ZStack(alignment: .bottomLeading) {
-            Image("DogCover")
-                .resizable()
-                .scaledToFill()
-            
-            Image("DogProfileImg")
-                .resizable()
-                .frame(width: 160, height: 160)
-                .clipShape(Circle())
-                .offset(x: 16, y: 30)
-            
-            ZStack{
-                Circle()
-                    .frame(width: 40, height: 40)
-                    .foregroundColor(Color(.lightGray))
+        VStack(alignment:.leading) {
+            ZStack(alignment: .bottomLeading) {
+                Image("DogCover")
+                    .resizable()
+                    .scaledToFill()
                 
-                Button {
-                    //change profile img
-                } label: {
-                    Image(systemName: "camera.fill")
+                Image("DogProfileImg")
+                    .resizable()
+                    .frame(width: 160, height: 160)
+                    .clipShape(Circle())
+                    .offset(x: 16, y: 30)
+                
+                ZStack{
+                    Circle()
                         .frame(width: 40, height: 40)
-                        .foregroundColor(.black)
-                        .overlay(Circle().stroke(Color.white, lineWidth: 2 ))
+                        .foregroundColor(Color(.lightGray))
                     
-                }
-            } .offset(x: 140, y: 20)
+                    Button {
+                        //change profile img
+                    } label: {
+                        Image(systemName: "camera.fill")
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.black)
+                            .overlay(Circle().stroke(Color.white, lineWidth: 2 ))
+                        
+                    }
+                } .offset(x: 140, y: 20)
+                
+            }
+            .frame(height: 96)
             
             
-            
-            
-            
-            
-            
-            
+            Spacer()
         }
-        .frame(height: 96)
         
     }
 }
