dc62b904222c9be256af84790ee7586611766790
Mato <Kittawat@icloud.com>
2022-12-04 22:50:18 +0800

    adjust profile header

--- AnimalWorld/Core/Profile/Views/ProfileView.swift
+++ AnimalWorld/Core/Profile/Views/ProfileView.swift
@@ -10,40 +10,18 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View {
         VStack(alignment:.leading) {
-            ZStack(alignment: .bottomLeading) {
-                Image("DogCover")
-                    .resizable()
-                    .scaledToFill()
-                
-                Image("DogProfileImg")
-                    .resizable()
-                    .frame(width: 160, height: 160)
-                    .clipShape(Circle())
-                    .offset(x: 16, y: 30)
-                
-                ZStack{
-                    Circle()
-                        .frame(width: 40, height: 40)
-                        .foregroundColor(Color(.lightGray))
-                    
-                    Button {
-                        //change profile img
-                    } label: {
-                        Image(systemName: "camera.fill")
-                            .frame(width: 40, height: 40)
-                            .foregroundColor(.black)
-                            .overlay(Circle().stroke(Color.white, lineWidth: 2 ))
+          
+            headerView
                         
-                    }
-                } .offset(x: 140, y: 20)
+            Text("dfsdf")
+            
+           
                 
-            }
-            .frame(height: 96)
             
-            
+     
             Spacer()
         }
-        
+        .ignoresSafeArea()
     }
 }
 
@@ -53,3 +31,56 @@ struct ProfileView_Previews: PreviewProvider {
     }
 }
 
+extension ProfileView {
+  
+    var headerView: some View {
+        ZStack(alignment: .bottomLeading) {
+            Image("Cover")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 300)
+                .clipped()
+                            
+            Image("DogProfileImg")
+                .resizable()
+                .frame(width: 160, height: 160)
+                .clipShape(Circle())
+                .offset(x: 16, y: 30)
+            
+            ZStack{
+                Circle()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(Color(.lightGray))
+                
+                Button {
+                    //change profile img
+                } label: {
+                    Image(systemName: "camera.fill")
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.black)
+                        .overlay(Circle().stroke(Color.white, lineWidth: 2 ))
+                    
+                }
+            } .offset(x: 140, y: 20)
+            
+            ZStack{
+                Circle()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(Color(.lightGray))
+                
+                Button {
+                    //change cover img
+                } label: {
+                    Image(systemName: "camera.fill")
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.black)
+                        .overlay(Circle().stroke(Color.white, lineWidth: 2 ))
+                    
+                }
+            } .offset(x: 360, y: 2)
+            
+            
+        }
+      
+    }
+}
